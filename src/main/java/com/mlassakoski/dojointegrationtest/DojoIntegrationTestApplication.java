09f5d2b8da5b451b8f10be2dbca076027804764b
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-09-28 20:19:59 -0300

    created main files

---
+++ src/main/java/com/mlassakoski/dojointegrationtest/DojoIntegrationTestApplication.java
@@ -0,0 +1,12 @@
+package com.mlassakoski.dojointegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoIntegrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoIntegrationTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mlassakoski/dojointegrationtest/DojoIntegrationTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mlassakoski.dojointegrationtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DojoIntegrationTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
