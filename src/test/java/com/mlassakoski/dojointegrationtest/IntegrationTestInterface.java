5c0d9a48955688e3f317eb27a2da04949bb669ca
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-10-02 21:06:10 -0300

    created integrationTestInterface

---
+++ src/test/java/com/mlassakoski/dojointegrationtest/IntegrationTestInterface.java
@@ -0,0 +1,21 @@
+package com.mlassakoski.dojointegrationtest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.*;
+
+@ActiveProfiles("integration-test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Inherited
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-integration-test.yaml")
+@Transactional
+public @interface IntegrationTestInterface {
+}
